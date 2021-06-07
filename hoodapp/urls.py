b60987c9cdacf37ddd39244599d888f6cde39f6b
Alicia-krynne <alicemacrineadhiambo@gmail.com>
2021-06-07 20:19:00 +0300

    add details

--- hoodapp/urls.py
+++ hoodapp/urls.py
@@ -3,19 +3,22 @@ from . import views
 from .views import *
 from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls import url,include
 
 
 urlpatterns=[
     path('',views.index,name='index'),
     path('signup/', views.signup, name='signup'),
-    path('profile/',views.profile,name = 'profile'),
+    #path('profile/',views.profile,name = 'profile'),
     path('profiles/)',views.profiles,name='profiles'),
     path('create/', views.create, name='create'),
     path('biz/', views.biz, name='biz'),
-    path('hood/', views.hood, name='hood'),
+    #path('hood/', views.hood, name='hood'),
     path('search/', views.search_results, name='search_results'),
     path('post/', views.post, name='post'),
-   
+     
+    url(r'^hood/(\d+)', views.hood, name='hood'),
+    url(r'^profile/',views.profile,name = 'profile'),
 ]
 
 
